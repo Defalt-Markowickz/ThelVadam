ece1eca406c9c02fb48f5f10e800ec0cd1a76f25
DefaltFUCK <85587928+DefaltFUCK@users.noreply.github.com>
2022-03-01 20:04:29 -0600

    Add files via upload

---
+++ Requeriments.sh
@@ -0,0 +1,14 @@
+apt update && apt upgrade
+sleep 3
+clear
+
+echo "A contuniacion se instalaran los requisitos :D"
+
+pkg install python3
+pip --upgrade install
+pip install colorama
+pip install os
+pip install time
+pip install base64
+sleep 3
+clear
